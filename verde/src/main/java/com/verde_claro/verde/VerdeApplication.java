25430319aeb1897f3e1ed582d77460e40e5954c7
Jhonny Gil <jhonny.gil@o365.unab.edu.co>
2022-12-05 16:09:42 -0500

    inicio proyecto ciclo 4

---
+++ verde/src/main/java/com/verde_claro/verde/VerdeApplication.java
@@ -0,0 +1,13 @@
+package com.verde_claro.verde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdeApplication.class, args);
+	}
+
+}
---
+++ verde/src/test/java/com/verde_claro/verde/VerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verde_claro.verde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
